5dd7c61deb46470956f9fafb2fc783cfd613a90a
ImmortalFae <96302915+ImmortalFae@users.noreply.github.com>
2023-05-12 05:37:04 -0700

    js1

---
+++ src/Javascript/script.js
@@ -0,0 +1,11 @@
+var salary  = prompt ("Sheiyvane tanxa");
+if(salary <=400){
+    console.log ("wai bidzia sxvagan")
+}
+else if (salary > 400 && salary < 800){
+    alert("sashualo xelpasi");
+}
+else{
+    alert("mec minda");
+} 
+
